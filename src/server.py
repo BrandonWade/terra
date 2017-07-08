89fe6663c28f5c106588481071d7b4e818f1f624
Brandon Wade <brandon_wade2005@hotmail.com>
2017-07-08 09:17:49 -0500

    Use a set instead of list for ignore list to speed up lookups.

--- src/server.py
+++ src/server.py
@@ -84,12 +84,12 @@ def get_images_from_gallery(path):
 def get_ignore_list():
     ignore_list = []
     ignore_path = os.path.join(GALLERY_DIR, 'ignore.txt')
-    with open(ignore_path) as file:
-        ignore_list = [line.strip() for line in file]
+    with open(ignore_path) as f:
+        ignore_list = {line.strip() for line in f}
 
     return ignore_list
 
 def ignore_file(name):
     ignore_path = os.path.join(GALLERY_DIR, 'ignore.txt')
-    with open(ignore_path, 'a') as file:
-        file.write(name + '\n')
+    with open(ignore_path, 'a') as f:
+        f.write(name + '\n')
